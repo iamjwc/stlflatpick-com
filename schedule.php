5aa1902bcc66ecb22d3b9e01a05d487acf9bdba3
Justin Camerer <iamjwc@gmail.com>
2009-06-13 11:22:37 -0400

    Now using ads partial

--- schedule.php
+++ schedule.php
@@ -130,20 +130,7 @@
 </table>
 </div>
 
-<div id="ads">
-  <ul>
-    <li><img src="images/ads/ghs.jpg" alt="GHS Strings" /></li>
-    <li><img src="images/ads/Proulxlogosmall.jpg" alt="" /></li>
-    <li><img src="images/ads/collings.gif" alt="" /></li>
-    <li><img src="images/ads/fgm.jpg" alt="" /></li>
-    <li><img src="images/ads/hussdalton.gif" alt="" /></li>
-    <li><img src="images/ads/intellitouch.jpg" alt="" /></li>
-    <li><img src="images/ads/mabc.jpg" alt="" /></li>
-    <li><img src="images/ads/musicfolk.jpg" alt="" /></li>
-    <li><img src="images/ads/winfieldlogo.jpg" alt="" /></li>
-  </ul>
-</div>
-
+<?php include("ads.php") ?>
 <?php include("naviation.php") ?>
 
 </body>

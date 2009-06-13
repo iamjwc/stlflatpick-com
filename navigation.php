ac8dcbb5b7e4147b3e5f073bc68ce2163a61c8bd
Justin Camerer <iamjwc@gmail.com>
2009-06-13 11:53:07 -0400

    Updated nav links

--- navigation.php
+++ navigation.php
@@ -1,11 +1,10 @@
 <div id="navigation">
 	<ul>
-    <li><a href="guitar_raffle.htm">Guitar Raffle</a></li>
-    <li><a href="register.htm">Register</a></li>
-    <li><a href="instructors.htm">Instructors</a></li>
-    <li><a href="lodging.htm">Lodging</a></li>
+    <li><a href="register.php">Register</a></li>
+    <li><a href="instructors.php">Instructors</a></li>
+    <li><a href="lodging.php">Lodging</a></li>
     <li><a href="http://maps.google.com/maps?q=11228+Lone+Eagle+Drive+Bridgeton,+MO+63044&ie=UTF8&om=1&z=15&iwloc=addr">Directions</a></li>
-    <li><a href="schedule.htm">Schedule</a></li>
+    <li><a href="schedule.php">Schedule</a></li>
     <li><a href="mailto:bullharman@cs.com">Contact Us</a></li>
 	</ul>
 </div>

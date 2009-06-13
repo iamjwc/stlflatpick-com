f58c93b05120ba230404a850282bda3e421e1938
Justin Camerer <iamjwc@gmail.com>
2009-06-13 11:25:13 -0400

    Added header partial

---
+++ header.php
@@ -0,0 +1,2 @@
+<h1><span>St. Louis Flatpick</span></h1>
+<h2>March 12<sup>th</sup>, 13<sup>th</sup>, and 14<sup>th</sup> 2010</h2>
--- index.php
+++ index.php
@@ -14,8 +14,7 @@
 
 <body>
 
-<h1><span>St. Louis Flatpick</span></h1>
-<h2>March 12<sup>th</sup>, 13<sup>th</sup>, and 14<sup>th</sup> 2010</h2>
+<?php include("header.php") ?>
 <?php include("naviation.php") ?>
 
 <div id="content">
